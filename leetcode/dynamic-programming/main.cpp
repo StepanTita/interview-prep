2a8861bc01ad742d3244ad7defb7d815c018e766
StepanTita <stepan.tytarenko@nure.ua>
2023-09-22 22:30:03 -0400

    add dp problems

--- leetcode/dynamic-programming/main.cpp
+++ leetcode/dynamic-programming/main.cpp
@@ -33,7 +33,7 @@ int climbStairs(int n) {
 }
 
 // 198. House Robber
-int rob(std::vector<int>& nums) {
+int rob(std::vector<int> &nums) {
     int n = nums.size();
 
     if (n == 1) {
@@ -104,10 +104,10 @@ int dfs(std::string &s, int i, TrieNode *curr, TrieNode *root, std::vector<int> 
     return (dp[i] > 0 || curr->is_terminal) ? PASS : FAIL;
 }
 
-bool wordBreak(std::string s, std::vector<std::string>& wordDict) {
+bool wordBreak(std::string s, std::vector<std::string> &wordDict) {
     TrieNode *root = new TrieNode();
 
-    for (auto &w : wordDict) {
+    for (auto &w: wordDict) {
         buildTrie(w, root);
     }
 
@@ -116,6 +116,118 @@ bool wordBreak(std::string s, std::vector<std::string>& wordDict) {
     return dfs(s, 0, root, root, dp);
 }
 
+// 894. All Possible Full Binary Trees
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+std::vector<TreeNode *> allPossibleFBT(int n) {
+    if (n % 2 == 0) return std::vector<TreeNode *>{};
+
+    std::vector<std::vector<TreeNode *>> dp(n + 1, std::vector<TreeNode *>());
+    dp[1] = std::vector<TreeNode *>{new TreeNode()};
+
+    for (int count = 1; count <= n; count += 2) {
+        for (int i = 1; i < count; ++i) {
+            int j = count - i - 1;
+            for (auto left: dp[i]) {
+                for (auto right: dp[j]) {
+                    auto root = new TreeNode(0, left, right);
+                    dp[count].emplace_back(root);
+                }
+            }
+        }
+    }
+
+    return dp[n];
+}
+
+// 1646. Get Maximum in Generated Array
+int getMaximumGenerated(int n) {
+    if (n < 1) return 0;
+
+    std::vector<long long> nums(n + 1, 0);
+    nums[1] = 1;
+
+    long long ans = 1;
+    for (int i = 1; i <= n / 2; ++i) {
+        nums[2 * i] = nums[i];
+
+        if (2 * i + 1 <= n) {
+            nums[2 * i + 1] = nums[i] + nums[i + 1];
+            ans = std::max(ans, nums[2 * i + 1]);
+        }
+    }
+
+    return ans;
+}
+
+// 1137. N-th Tribonacci Number
+int tribo(int n, std::vector<int> &dp) {
+    if (dp[n] != -1) return dp[n];
+
+    return dp[n] = tribo(n - 1, dp) + tribo(n - 2, dp) + tribo(n - 3, dp);
+}
+
+int tribonacci(int n) {
+    if (n <= 0) return 0;
+
+    if (n <= 2) return 1;
+
+    std::vector<int> dp(n + 1, -1);
+    dp[0] = 0;
+    dp[1] = 1;
+    dp[2] = 1;
+
+    return tribo(n, dp);
+}
+
+// 300. Longest Increasing Subsequence
+int lengthOfLIS(std::vector<int> &nums) {
+    int n = nums.size();
+
+    std::vector<int> dp(n + 1, INF);
+    dp[0] = -INF;
+
+    int ans = 1;
+
+    for (int i = 0; i < n; ++i) {
+        int j = std::lower_bound(dp.begin(), dp.end(), nums[i]) - dp.begin();
+        if (nums[i] > dp[j - 1] && nums[i] < dp[j]) {
+            dp[j] = nums[i];
+            ans = std::max(ans, j);
+        }
+    }
+
+    return ans;
+}
+
+// 322. Coin Change
+
+const int MAX_AMT = 1e4 + 1;
+
+int coinChange(std::vector<int> &coins, int amount) {
+    std::vector<int> dp(MAX_AMT, INF);
+
+    dp[0] = 0;
+    for (int i = 0; i < amount; ++i) {
+        for (auto c: coins) {
+            if ((long long) i + c >= MAX_AMT) continue;
+            dp[i + c] = std::min(dp[i + c], dp[i] + 1);
+        }
+    }
+
+    return dp[amount] != INF ? dp[amount] : -1;
+}
 
 int main() {
     auto v = std::vector<std::string>{"a", "b", "bbb", "bbbb"};
